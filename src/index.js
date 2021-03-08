16f08d0d26499ab3de728e507b0165852abdbbca
Kseniya Barkova <kcancer17@gmail.com>
2021-03-08 19:10:22 +0200

    fix: fix the solution

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ module.exports = function towelSort (matrix) {
   return matrix.reduce((result, arr, index) => {
     arr = index%2 === 0 ? arr : arr.reverse()
     return result.concat(arr)
-	},[]);
+	}, []);
 }
 
